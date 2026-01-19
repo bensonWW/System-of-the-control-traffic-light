37e43ae74da6682c4d4825908ecb21d1a0b1451d
bensonWW <tt23912681@gmail.com>
2026-01-19 17:23:48 +0800

    getEdgesVolume:取每一條路的Vol fixtheRoadData:補足沒有資料的車流
    演算法:利用output.rou取得所有的路徑，再利用之前寫的select()取得api得到的資料，計算車流量，最後利用sumolib可以得到outgoing也就是這條路出去連到的所有道路，最後把原本有資料的路徑*0.35加到沒有資料的路徑
    
    ps:因為怕無限迴圈我只讓他跑了800次

--- tools/fixRoadData.py
+++ tools/fixRoadData.py
@@ -9,38 +9,52 @@ def getMapData():
     for child in root:
         if(child.tag == "edge"):
             if("function" not in child.attrib):
-                temp[child.attrib["id"]] = [child.attrib["from"], child.attrib["to"], False]
+                temp[child.attrib["id"]] = [child.attrib["from"], child.attrib["to"]]
     return temp
-def markMapData(mapData):
+def getEdgesVolume():
+    finalInfo = {}
     path = "./data/output.rou.xml"
-    tree = ET.parse(path)
-    tripInfo = tree.getroot()
-    for edgeId in mapData:
-        for info in tripInfo:
-            if(info.tag == "vehicle"):
-                route = info.find("route")
-                edges = route.attrib["edges"].split(" ")
-                for e in edges:
-                    if(e == edgeId):
-                        mapData[edgeId][2] = True
-                        break
-    return mapData
+    tripRoot = ET.parse(path).getroot()
+    roadInfo = ST.select() #'八德路    松江路-忠孝東路': {'SectionId': 'ZJTJ960', 'AvgSpd': '45.435482', 'AvgOcc': '1.6', 'TotalVol': '62.0', 'MOELevel': '0', 'StartWgsX': 392.3516351377892, 'StartWgsY': 366.40778006647395, 'EndWgsX': 105.22111980907044, 'EndWgsY': 285.28874187925237, 'from': 'cluster_2528018119_655375236', 'to': '655375228'}
+    for info in tripRoot:
+        if(info.tag == "vehicle"):
+                fromNode = info.attrib["fromTaz"]
+                toNode = info.attrib["toTaz"]
+                passRoads = info.find("route").attrib["edges"].split(" ")
+        else:
+            continue
+        for name in roadInfo:
+            if(roadInfo[name]["from"] == fromNode and roadInfo[name]["to"] == toNode):
+                for edgeId in passRoads:
+                    if edgeId not in finalInfo:
+                        finalInfo[edgeId] = [int(float(roadInfo[name]["TotalVol"])),1]
+                    else:
+                        finalInfo[edgeId][0] += int(float(roadInfo[name]["TotalVol"]))
+                        finalInfo[edgeId][1] += 1
+    for edgeId in finalInfo:
+        finalInfo[edgeId] = finalInfo[edgeId][0] // finalInfo[edgeId][1]
+    return finalInfo
 def fixtheRoadData():
-    mapData = markMapData(getMapData())
-    path = "./data/ntut-the way.net.xml"
-    tripPath = "./data/output.rou.xml"
-    root = ET.parse(tripPath).getroot()
-    net = SLB.net.readNet(path)
-    temp = {}
+    ALPHA = 0.35
+    MAX_ITERS = 800
+    net_path = "./data/ntut-the way.net.xml"
+    mapData = getMapData()
+    edgesVolume = getEdgesVolume()
+    net = SLB.net.readNet(net_path)
     for edgeId in mapData:
-        vol = 0
-        if(mapData[edgeId][2] == False):
+        if edgeId not in edgesVolume:
+            edgesVolume[edgeId] = 0
+    for iter in range(MAX_ITERS):
+        for edgeId in edgesVolume:
             edge = net.getEdge(edgeId)
-            fromNode = edge.getFromNode()
-            toNode = edge.getToNode()
-            print()
-            vol +=  ST.select()[fromNode]["TotalVol"]
-            vol +=  ST.select()[toNode]["TotalVol"]
-    return temp
+            outgoingEdgesId = edge.getToNode().getOutgoing()
+            for outEdge in outgoingEdgesId:
+                if edgesVolume[outEdge.getID()] == 0:
+                    edgesVolume[outEdge.getID()] += int(edgesVolume[edgeId] * ALPHA)
+    return edgesVolume
 if __name__ == "__main__":
-    print(fixtheRoadData())
+    edgesVolume = fixtheRoadData()
+    print(edgesVolume)
+    for edgeId in edgesVolume:
+        if edgesVolume[edgeId] == 0:
+            print(edgeId)
